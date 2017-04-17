98d3ac12a2419b2ad3b6734a6fec88cabbb20f88
Aviad Cohen <aviadcohen7@gmail.com>
2017-04-17 13:03:58 +0300

    Bug fix in Honeypot class - isChnaged dosen't work well #6
    Signed-off-by: Aviad Cohen <aviadcohen7@gmail.com>

--- AntiRansomware.cpp
+++ AntiRansomware.cpp
@@ -4,6 +4,9 @@
 #include "stdafx.h"
 
 #include "HoneypotNameGenerator.h"
+#include "Honeypot.h"
+#include <fstream>
+#include <iostream>
 
 using std::wstring;
 using std::list;
@@ -11,6 +14,7 @@ using std::list;
 int main()
 {
 	list<wstring> checkList = HoneypotNameGenerator::createFullFileNames();
+
 	log().info(__FUNCTION__, "Stating Main!!!");
 
 	for (list<wstring>::iterator it = checkList.begin(); it != checkList.end(); ++it)
--- Honeypot.cpp
+++ Honeypot.cpp
@@ -2,14 +2,15 @@
 
 #include "Honeypot.h"
 #include "Logger.h"
+#include <string>
 
 using namespace std;
 
-#define FILE_CONTENT L"@NOAM YOU MAKE ME CRAZY AHHHHHHHHHHHHH 01234\n"
+#define FILE_CONTENT L"@The is the original content of the Honeypot\n"
 
-#define MAX_LENGTH_READ_FILE_CONTENT ((sizeof(FILE_CONTENT) + 1024) * sizeof(wchar_t))
+#define LENGTH_FILE_CONTENT (sizeof(FILE_CONTENT))
 
-/* This class handles the Honeyput file status and maintainess */
+/* This class handles the Honeypot file status and maintainers */
 Honeypot::Honeypot(LPCWSTR lpFileName): lpFileName(lpFileName){
 }
 
@@ -26,7 +27,7 @@ const wstring Honeypot::getFileName() {
 DWORD Honeypot::create() {
 	HANDLE fileHandle = CreateFile(lpFileName.c_str(), GENERIC_READ | GENERIC_WRITE,
 		FILE_SHARE_DELETE | FILE_SHARE_READ | FILE_SHARE_WRITE,
-		NULL, CREATE_NEW, FILE_ATTRIBUTE_SYSTEM | FILE_FLAG_SEQUENTIAL_SCAN, NULL);
+		NULL, CREATE_NEW, FILE_ATTRIBUTE_HIDDEN | FILE_FLAG_SEQUENTIAL_SCAN, NULL);
 
 	if (fileHandle == INVALID_HANDLE_VALUE) {
 		log().error(__FUNCTION__, L"CreateFile failed to open Honeypot " + lpFileName + L", errno: " + to_wstring(GetLastError()));
@@ -34,9 +35,9 @@ DWORD Honeypot::create() {
 		return GetLastError();
 	}
 
-	if (!WriteFile(fileHandle, FILE_CONTENT, wcslen(FILE_CONTENT), NULL, NULL)) {
+	if (!WriteFile(fileHandle, FILE_CONTENT, LENGTH_FILE_CONTENT, NULL, NULL)) {
 		/* Write content to file failed */
-		log().error(__FUNCTION__, L"CreateFile failed to write content to Honeyput " + lpFileName + L", errno: " + to_wstring(GetLastError()));
+		log().error(__FUNCTION__, L"CreateFile failed to write content to Honeypot " + lpFileName + L", errno: " + to_wstring(GetLastError()));
 		log().error(__FUNCTION__, L"Removing Honeypot from system.");
 
 		DeleteFile(lpFileName.c_str());
@@ -50,11 +51,11 @@ DWORD Honeypot::create() {
 }
 
 bool Honeypot::isChanged() {
-	wchar_t readContent[MAX_LENGTH_READ_FILE_CONTENT];
+	wchar_t readContent[LENGTH_FILE_CONTENT + 1024];
 
 	HANDLE fileHandle = CreateFile(lpFileName.c_str(), GENERIC_READ,
 		FILE_SHARE_DELETE | FILE_SHARE_READ | FILE_SHARE_WRITE,
-		NULL, OPEN_EXISTING, FILE_ATTRIBUTE_SYSTEM | FILE_FLAG_SEQUENTIAL_SCAN, NULL);
+		NULL, OPEN_EXISTING, FILE_ATTRIBUTE_HIDDEN | FILE_FLAG_SEQUENTIAL_SCAN, NULL);
 
 	if (fileHandle == INVALID_HANDLE_VALUE) {
 		/* File creation failed */
@@ -63,7 +64,7 @@ bool Honeypot::isChanged() {
 		return true;
 	}
 
-	if (!ReadFile(fileHandle, readContent, MAX_LENGTH_READ_FILE_CONTENT, NULL, NULL)) {
+	if (!ReadFile(fileHandle, readContent, LENGTH_FILE_CONTENT, NULL, NULL)) {
 		log().error(__FUNCTION__, L"Failed to read Honeypot " + lpFileName + L" to check modifications.");
 
 		CloseHandle(fileHandle);
